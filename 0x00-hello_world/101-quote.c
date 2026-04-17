b429ae19fa0b5a8a6da64c66c0be4e76e70d26d1
Shalom-227 <alaladeshalom02@gmail.com>
2026-04-17 17:51:44 +0100

    feat: add program that prints a string to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+/**
+ * Author: Shalom Alalade
+ * Program: WinMingle Community C Training
+ * Description: Write a C program that prints exactly:
+and that piece of art is useful" - Dora Korpar, 2015-10-19
+
+to the standard error.
+
+ */
+
+
+#include<stdio.h>
+
+int main(void)
+{
+    
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\");
+    return 1;
+}
+
+ 
